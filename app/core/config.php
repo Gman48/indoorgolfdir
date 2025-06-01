7a8e364248573b609e15e47a80de8d283d175c81
Gman48 <116597720+Gman48@users.noreply.github.com>
2025-05-31 20:57:42 -0400

    Update config.php

--- app/core/config.php
+++ app/core/config.php
@@ -13,13 +13,13 @@ if($_SERVER['SERVER_NAME'] == "localhost")
 
 }else{
 	//for online server
-	define("ROOT", "http://www.ezlaunchweb.com");	
+	define("ROOT", "http://www.indoorgolfdir.com");	
 
 	define("DBDRIVER", "mysql");
 	define("DBHOST", "localhost");
 	define("DBUSER", "u630753873_webmaster");
 	define("DBPASS", "IamMaster!!1");
-	define("DBNAME", "u630753873_atctest");
+	define("DBNAME", "u630753873_indoorgolf");
 }
 
 define('APP_NAME', "Indoor Golf Directory");
