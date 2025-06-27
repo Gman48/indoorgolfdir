ae5332a690fe92dd0f6140fec59c639084f8bb01
Gman48 <116597720+Gman48@users.noreply.github.com>
2025-06-27 11:42:11 -0400

    Update hero.php

--- app/pages/includes/hero.php
+++ app/pages/includes/hero.php
@@ -5,7 +5,7 @@
           <h1 class="headline">The Indoor Golf Directory</h1>
           <h3 class="sub-text">Looking to keep your game sharp during the off season?</h3>
           <h3 class="sub-text">Just want to scratch that golfing itch?</h3>
-          <h3 class="sub-text sub-text-blk">Our directory of indoor golf facilities in North America is just the tool you need.  Easily find an indoor facility anywhere in <span class="red">Canada</span> or the <span class="blue">United States</span>.</h3>
+          <h3 class="sub-text sub-text-blk">Our directory of indoor golf facilities and simulators in North America is just the tool you need.  Easily find a place to play indoor golf anywhere in <span class="red">Canada</span> or the <span class="blue">United States</span>.</h3>
       <!-- </div> -->
   </div>
 </div>
