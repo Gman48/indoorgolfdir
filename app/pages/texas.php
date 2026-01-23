76aaf58fb4e8cb0b80b606f8c2fdc053c71b6fdc
Gman48 <116597720+Gman48@users.noreply.github.com>
2026-01-23 16:43:34 -0500

    Create texas.php

---
+++ app/pages/texas.php
@@ -0,0 +1,72 @@
+<?php require page('includes/header');?>
+
+<!-- Hero Section -->
+<div id="hero-bg" class="hero-bg-state">
+    <h1 class="headline-state">Indoor Golf Facilities</h1>
+    <h1 class="headline">Texas</h1>
+    <h3 class="sub-text">What region of Texas are you looking in?</h3>
+
+    <div class="region-btns">
+        <a href="<?=ROOT?>/dynamic_region?region_id=81" class="region-btn">Dallas-Fort Worth</a>
+        <a href="<?=ROOT?>/dynamic_region?region_id=82" class="region-btn">Houston</a>
+        <a href="<?=ROOT?>/dynamic_region?region_id=83" class="region-btn">Austin</a>
+        <a href="<?=ROOT?>/dynamic_region?region_id=84" class="region-btn">San Antonio</a>
+        <a href="<?=ROOT?>/dynamic_region?region_id=85" class="region-btn">Other (rural TX)</a>
+    </div>
+</div>
+
+<div class="container">
+    <p class="state-summary">Texas is home to over 100 indoor golf facilities with golf simulators that let you enjoy the game year round. Search by clicking on a region above or check out our featured Texas facilities below.</p>
+</div>
+
+<!-- Listings Section -->
+<section class="container">
+    <div class="listings-grid">
+        <div class="listings-grid-col1">
+        
+            <?php 
+                $query = "select * from allfacilities where state = :state and featured = 1";
+                $rows = db_query($query, ['state'=>'tx']);
+            ?>
+
+            <?php if(!empty($rows)):?>
+            <?php foreach($rows as $row):?>
+
+            <div class="listings-container">
+                <img class="listings-img" src="<?=ROOT?>/<?=$row['facility_img']?>" alt="facility image">
+                <div class="listings-summary">
+                    <div class="name-featured">
+                        <div><a class="facilitylink facility-name" href="<?=ROOT?>/facility_details?facility_id=<?=$row['facility_id']?>"><?=esc($row['facility_name'])?></a></div>
+                    </div>
+                    <div class="facility-address">
+                        <?=esc($row['facility_street'])?>, <?=esc($row['facility_city'])?>
+                    </div>
+                    <!-- <a class="weblink" href="<?=$row['website_link']?>"><?=esc($row['website_short'])?></a> -->
+                    <div class="facility-amenities"> 
+                        <?php if($row['lessons']) {echo('lessons, ');}?>
+                        <?php if($row['bar']) {echo('licensed lounge, ');}?>
+                        <?php if($row['food']) {echo('snack bar, ');}?>
+                        <?php if($row['leagues']) {echo('leagues, ');}?>
+                        <?php if($row['24hrs']) {echo('open 24 hours ');}?>
+                    </div> 
+                </div>
+                <div class="featured-label">
+                    <?php if($row['featured']) echo 'Featured Listing' ?>
+                </div>
+            </div>
+            <hr class="list-break">
+            <?php endforeach;?>
+            <?php endif;?>
+        </div>
+
+        <div class="listings-grid-col2">
+            <div class="state-right-sidebar">
+                <div class="ad-grid-item">Add space</div>
+                <div class="ad-grid-item">Second Add space</div>
+                <div class="ad-grid-item">Third Add space</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require page('includes/footer');?>
